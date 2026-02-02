5c878f330b226e7aae9fd68687c13d1c7812c3cf
roger1145141919810 <chengchih1110@gmail.com>
2026-02-02 17:01:07 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,34 +2,25 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
+const { generateDeck, shuffle, deal } = require('./game/engine'); // 引用你的 engine.js
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// 提供 public 靜態檔案
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// 引入遊戲引擎邏輯
-const { generateDeck, shuffle, deal } = require('./game/engine');
-
-// 用來儲存房間狀態的物件
-const rooms = {};
+const rooms = {}; // 儲存房間狀態
 
 io.on('connection', (socket) => {
     console.log('玩家連線:', socket.id);
 
-    // 1. 建立房間
     socket.on('create_room', ({ roomId, name }) => {
         socket.join(roomId);
-        if (!rooms[roomId]) {
-            rooms[roomId] = { players: [] };
-        }
-        rooms[roomId].players.push({ id: socket.id, name });
+        rooms[roomId] = { players: [{ id: socket.id, name }], gameStarted: false };
         io.to(roomId).emit('room_update', rooms[roomId].players);
     });
 
-    // 2. 加入房間
     socket.on('join_room', ({ roomId, name }) => {
         if (rooms[roomId]) {
             socket.join(roomId);
@@ -40,39 +31,23 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 3. 開始遊戲並發牌
     socket.on('start_game', ({ roomId }) => {
         if (!rooms[roomId]) return;
-        
-        const deck = shuffle(generateDeck());
+        const deck = shuffle(generateDeck()); // 使用你的 engine.js 功能
         const hands = deal(deck, rooms[roomId].players.length);
         
-        // 分別給每個玩家發手牌
-        rooms[roomId].players.forEach((player, index) => {
-            io.to(player.id).emit('deal', hands[index]);
+        rooms[roomId].players.forEach((player, i) => {
+            io.to(player.id).emit('deal', hands[i]); // 發牌給個別玩家
         });
-
         io.to(roomId).emit('game_start');
     });
 
-    // 4. 處理出牌 (這是你目前 client.js 最需要的)
     socket.on('play_cards', ({ roomId, cards }) => {
-        console.log(`房間 ${roomId} 玩家 ${socket.id} 出牌:`, cards);
-        // 這裡暫時不做規則判定，直接廣播給所有人桌上的新牌
-        io.to(roomId).emit('play_made', { playerId: socket.id, cards });
-    });
-
-    // 5. 處理過牌 (Pass)
-    socket.on('pass', ({ roomId }) => {
-        io.to(roomId).emit('status_update', `玩家 ${socket.id} 選擇過牌`);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('玩家離開:', socket.id);
+        io.to(roomId).emit('play_made', { playerId: socket.id, cards }); // 廣播出的牌
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => { // 確保 Render 能正常監聽
+    console.log(`Server running on port ${PORT}`);
 });
